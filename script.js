55f146a9a4093c114b4427ace9c6661c42569135
Seyeon-Jo <seyeonjo.ud@gmail.com>
2023-02-13 17:02:30 -0500

    finish dashboard project

--- script.js
+++ script.js
@@ -4,14 +4,36 @@ const body = document.querySelector("body"),
   sidebar = body.querySelector("nav"),
   sidebarToggle = body.querySelector(".sidebar-toggle");
 
-// Start toggle dark-mode
+// Start the setting to keep the user selected mode & sidebar
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+  body.classList.toggle("dark");
+}
+
+let getStatus = localStorage.getItem("status");
+if (getStatus && getStatus === "close") {
+  sidebar.classList.toggle("close");
+}
+// End the setting to keep the user selected mode & sidebar
+
+// Start toggle dark-mode & keep the user selected mode
 modeToggle.addEventListener("click", () => {
   body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("mode", "dark");
+  } else {
+    localStorage.setItem("mode", "light");
+  }
 });
-// End toggle dark-mode
+// End toggle dark-mode & keep the user selected mode
 
-// Start toggle sidebar
+// Start toggle sidebar & keep the user selected sidebar
 sidebarToggle.addEventListener("click", () => {
   sidebar.classList.toggle("close");
+  if (sidebar.classList.contains("close")) {
+    localStorage.setItem("status", "close");
+  } else {
+    localStorage.setItem("status", "open");
+  }
 });
-// End toggle sidebar
+// End toggle sidebar & keep the user selected sidebar
